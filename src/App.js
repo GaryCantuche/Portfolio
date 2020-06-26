a189c7687defe23e93e9a48b95e7cf5cf7380a2f
GaryCantuche <garycantuche2000@gmail.com>
2020-06-26 17:37:18 +0200

    NavBar constructed: Declared the navbar and router of the page. Starting with the grid of homepage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 //import $ from 'jquery';
 //import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import NavBar from './Components/NavBar/navbar';
+import Home from './pages/home/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" >
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/home/home.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from "./../../images/Logo.png";
+function Home() {
+    return (
+        <div className="container-fluid border">
+            <div className="row">
+                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12  border no-gutters">
+                    <div>
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                    </div>
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 border">
+                    <img className="img-fluid" src={logo} alt="That I'm I" />
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 border">
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                    pepe macarenofgmkwpeofmkwepomfowpemfpwoemfwefmwe
+                    fwef
+                    wef
+                    we
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
